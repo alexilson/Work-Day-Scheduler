041c03c4c890fb75b043f8dc6986b4fe988aa559
Alex <ilson.alex@gmail.com>
2023-10-20 19:25:33 -0700

    Added event handler for save button, added current day name to top of page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,6 +8,17 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  let currentDayEl = $('#currentDay');
+  let saveButtonEl = $('.saveBtn');
+  
+  // display name of the current day at top of page
+  currentDayEl.text(dayjs().format('dddd'));
+
+  // Event listener for save button click
+  saveButtonEl.on('click', function() {
+    
+  });
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
